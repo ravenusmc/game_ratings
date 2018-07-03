1b474ce6349356e8c309b02cc893b89f7dbdf561
ravenusmc <mcuddy77@gmail.com>
2018-07-03 10:29:55 -0400

    added a home page

--- project/site/routes.py
+++ project/site/routes.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, render_template
+from project.site.test import *
+
 
 mod = Blueprint('site', __name__,template_folder='templates', static_folder='static',
                                                 static_url_path='/static')
 
-
 @mod.route('/')
 def homepage():
-  return render_template('site/index.html')
+  test = Test()
+  name = test.test()
+  return render_template('site/index.html', name = name)
 
 @mod.route('/home')
 def home():
---
+++ project/site/test.py
@@ -0,0 +1,8 @@
+class Test():
+
+  def test(self):
+    name = "Mike"
+    return name
+
+# test = Test()
+# test.test()
