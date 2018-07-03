227a5df13103f3e1a1d930f1e39c827914fb2d9f
ravenusmc <mcuddy77@gmail.com>
2018-07-02 20:01:45 -0400

    restructuring site to work with flask blueprints

--- project/site/routes.py
+++ project/site/routes.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, render_template
 
-mod = Blueprint('site', __name__,template_folder='templates')
-mod = Blueprint('site', __name__,static_folder='templates')
+mod = Blueprint('site', __name__,template_folder='templates', static_folder='static',
+                                                static_url_path='/static')
+
 
 @mod.route('/')
 def homepage():
   return render_template('site/index.html')
+
+@mod.route('/home')
+def home():
+    return render_template('site/home.html')
