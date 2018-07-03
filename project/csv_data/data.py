f5cb49d5fcad5ba770ed6b5c90762d1af13016be
ravenusmc <mcuddy77@gmail.com>
2018-07-03 17:21:28 -0400

    started work on handling csv data

---
+++ project/csv_data/data.py
@@ -0,0 +1,21 @@
+#This file will deal with all of the data that this project will analyze from the csv file 
+
+#Bringing in the files that I need:
+# from bokeh.charts import Scatter, output_file, show
+# from bokeh.plotting import figure, output_file, show
+import csv
+from csv import writer
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+class Data():
+
+    def __init__(self):
+        self.game_data = pd.read_csv('.data/Video_Games_Sales.csv')
+
+    def test(self):
+        print(self.game_data.head())
+
+
+
