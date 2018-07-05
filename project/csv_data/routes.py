6183ec4e1b87684284f6fd23dc867ef81a103810
ravenusmc <mcuddy77@gmail.com>
2018-07-05 14:38:52 -0400

    wrote function for ajax call

--- project/csv_data/routes.py
+++ project/csv_data/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, Flask, session, jsonify, redirect, url_for, escape, render_template, request, flash
 from project.csv_data.data import * 
 
 mod = Blueprint('csv_data', __name__,template_folder='templates', static_folder='static',
@@ -7,3 +7,14 @@ mod = Blueprint('csv_data', __name__,template_folder='templates', static_folder=
 @mod.route('/csv_home')
 def homepage():
   return render_template('csv_data/csv_index.html')
+
+
+#AJAX Functions below this line 
+
+#This route will deal with getting the max rating of a video game by year and genre 
+@mod.route('/_by_max_rating')
+def by_state_shape():
+    year = request.args.get('year', 0, type=int)
+    genre = request.args.get('genre', 0, type=str)
+    result = genre
+    return jsonify(result = result)
--- project/static/js/csvData.js
+++ project/static/js/csvData.js
@@ -11,5 +11,22 @@ function validateYear() {
         message = "Input is not valid";
         alert(message);
     }
-
 }
+
+//The functions below will deal with the jquery on the page 
+$(function() {
+
+    var submit_form = function(e) {
+        //The /_by_state is the method that you will use.
+      $.getJSON($SCRIPT_ROOT + '/_by_max_rating', {
+        year: $('input[name="year"]').val(),
+        genre: $('input[name="genre"]').val()
+      }, function(data) {
+        //This is where the data will be displayed.
+        $('#max_rating_results').text(data.result);
+      });
+      return false;
+    };
+    //This is what will submit the form when the user clicks the link.
+    $('a#max_rating').bind('click', submit_form);
+});
