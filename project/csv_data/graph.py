d6838fc659495325c775ab9edc7bef0a90a7ca64
ravenusmc <mcuddy77@gmail.com>
2018-07-14 15:53:09 -0400

    added all playstation graphs

--- project/csv_data/graph.py
+++ project/csv_data/graph.py
@@ -59,7 +59,7 @@ class Graph():
         #Creating an output file 
         output_file("game_count.html")
 
-        file = 'X360.csv'
+        file = 'PS.csv'
 
         game_data = pd.read_csv(file)
 
