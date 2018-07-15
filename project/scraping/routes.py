ca42329107efddce8e0fdfc569813ecfe797ddb0
ravenusmc <mcuddy77@gmail.com>
2018-07-15 15:42:22 -0400

    added to the form to receive game title data

--- project/scraping/routes.py
+++ project/scraping/routes.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, Flask, session, jsonify, redirect, url_for, escape, render_template, request, flash
 
 mod = Blueprint('scraping', __name__, template_folder='templates', static_folder='static',
                                                 static_url_path='/static')
 
-@mod.route('/scraping_home')
+@mod.route('/scraping_home', methods=['GET', 'POST'])
 def scraping_homepage():
-  return render_template('scraping/scraping.html')
+    if request.method == 'POST':
+        #Recieving the information from the user.
+        gameTitle = request.form['gameTitle']
+        print(gameTitle)
+    return render_template('scraping/scraping.html')
