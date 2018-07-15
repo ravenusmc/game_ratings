b3eeb7bf7bef165fb17a6d221619e45fa9175dcd
ravenusmc <mcuddy77@gmail.com>
2018-07-15 15:25:39 -0400

    started work on scraping page

---
+++ project/scraping/scraping.py
@@ -0,0 +1,9 @@
+#This file will contain all of the code for the scraping of the metacritic website 
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+url="http://www.metacritic.com/game/playstation-4/god-of-war"
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+web_byte = urlopen(req).read()
+bs = BeautifulSoup(web_byte,"html.parser")
+print(bs)
